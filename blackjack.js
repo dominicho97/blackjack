52bca912b1050b01617ace339f07c6864a5b05bb
dominicho97 <dominic.ho97@gmail.com>
2018-11-01 15:07:27 +0100

    added let and console

--- blackjack.js
+++ blackjack.js
@@ -2,3 +2,14 @@
 // blackjack
 // y Dominic Ho
 
+
+let card1 = "Ace of spades "
+    card2 = "Ten of hearts"
+
+    console.log("Welcome to Blackjack!");
+
+    console.log("You are dealt: ");
+
+    console.log("  "+ card1);
+    console.log("  "+ card2);
+
