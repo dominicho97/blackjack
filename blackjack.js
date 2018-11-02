0bb7fc1483d5c2080710762a107dbfdc1c66e647
dominicho97 <dominic.ho97@gmail.com>
2018-11-02 19:36:16 +0100

    added let suits variable

--- blackjack.js
+++ blackjack.js
@@ -2,6 +2,11 @@
 // blackjack
 // y Dominic Ho
 
+
+let suits ['Hearts, Clubs, Diamonds, Spades' ];
+
+
+
 let deck = ["Ace of spades ",
             "Two of spades ",
             "Three of spades"
@@ -9,13 +14,14 @@ let deck = ["Ace of spades ",
 ];
 
 
-let playerCards = [deck[0], deck][2];
+let playerCards = [deck[0], deck[2]];
 
 
 
     console.log("Welcome to Blackjack!");
     console.log("You are dealt: ");
 
+    console.log("  "+ playerCards[1]);
     console.log("  "+ playerCards[0]);
-    console.log("  "+ playerCards[0]);
+
 
