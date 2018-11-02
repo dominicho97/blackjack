179a8911aa38bc1f8024393dbf2dcbd60b0bea29
dominicho97 <dominic.ho97@gmail.com>
2018-11-02 19:57:15 +0100

    cleaned code

--- blackjack.js
+++ blackjack.js
@@ -3,15 +3,15 @@
 // y Dominic Ho
 
 
-let suits = ['Hearts, Clubs, Diamonds, Spades' ];
-let values = ['Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two, ']
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades' ];
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two']
 
 let deck = [];
 
 
 for (let suitIdx = 0; suitIdx<suits.length; suitIdx++) {
 for (let valuesIdx = 0; valuesIdx<values.length;valuesIdx++) {
-deck.push(values[valuesIdx]) + '' of '' + suits[suitIdx]);
+deck.push(values[valuesIdx] + ' of ' + suits[suitIdx] );
   }
 }
 
