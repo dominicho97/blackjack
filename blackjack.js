e78542c4f1f4ad5ad18ebd2a0c6b5bb8f39def9a
dominicho97 <dominic.ho97@gmail.com>
2018-11-04 00:30:22 +0100

    added function updateScores

--- blackjack.js
+++ blackjack.js
@@ -84,6 +84,14 @@ function getNextCard(){
   return deck.shift();
 }
 
+
+function updateScores(){
+  dealerScore = getScore(dealerCards);
+  playerScore = getScore(playerCards);
+}
+
+
+
 function showStatus(){
   if (!gameStarted){
     textArea.innerText = 'Welcome to Blackjack!';
@@ -95,6 +103,42 @@ let dealerCardString = '';
 for(let i=0; i<dealerCards.length; i++){
   playerCardString += getCardString(playerCards[i]) + '/n';
 }
+
+let playerCardString  = '';
+for (let i= 0; i< playerCards.length; i++){
+  dealerCardString += getCardString(playerCards[i]) + '/n';
+}
+
+updateScores();
+
+
+textArea.innerText = 
+'Dealer has: /n'+
+dealerCardString +
+'(score:' + playerScore + ')/n/n';
+
+'Player has:/n' +
+playerCardString +
+'(score:'+ playerScore + ')/n/n';
+
+if (gameOver){
+  if (playerWon){
+textArea.innerText += "YOU WIN!";
+  }  else {
+    textArea.innerText +="DEALER WINS!";
+  }
+newGameButton.style.display = 'inline';
+hitButton.style.display = 'none';
+stayButton.style.display = 'none';
+
+
+
+}
+
+
+
+
+
 
 
 
