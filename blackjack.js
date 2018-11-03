47d352402fc1c0e2df7046b8b3e6ba580b3ec9b4
dominicho97 <dominic.ho97@gmail.com>
2018-11-04 00:44:29 +0100

    added function getCardNumericalValue

--- blackjack.js
+++ blackjack.js
@@ -85,6 +85,67 @@ function getNextCard(){
 }
 
 
+function getCardNumericalValue(card){
+switch(card.value){
+  case'Ace':
+  return 1
+  case'Two':
+  return 2
+  case'Three':
+  return 3
+  case 'Four':
+  return 4
+  case 'Five':
+  return 5
+  case 'Six':
+  return 6
+  case 'Seven':
+  return 7
+  case'Eight':
+  return 8
+  case 'Nine':
+  return 9
+   default:
+  return 10 
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+function getScore(cardArray){
+ let score = 0;
+ let hasAce = false;
+ for(let i = 0;  i<cardArray.length; i++){
+   let card = cardArray[i];
+   score += getCardNumericalValue(card);
+   if (card .value === 'Ace'){
+     hasAce = true;
+   }
+ }
+if (hasAce && score + 10 <= 21){
+  return score + 10;
+  }
+  return score;
+}
+
+
+
+
+
+
+
+
+
 function updateScores(){
   dealerScore = getScore(dealerCards);
   playerScore = getScore(playerCards);
